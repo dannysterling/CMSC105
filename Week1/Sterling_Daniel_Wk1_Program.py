c07c2fb7a4fadcafc7ad83c2b3e4c5a53393b96f
Daniel Sterling <dlsterling2000@gmail.com>
2024-10-19 17:20:12 -0400

    Add files via upload

---
+++ Week1/Sterling_Daniel_Wk1_Program.py
@@ -0,0 +1,17 @@
+# Daniel Sterling
+# October 10, 2024
+# Week 1 Assignment 2: Programming Assignment
+
+# Input Section
+author_name = "Slobodan Pupovac" # Author's name
+
+# Haiku phrases
+first_phrase = "top of the mountain" # First phrase
+second_phrase = "at the valley the clouds float" # Second phrase
+third_phrase = "by a calm river" # Third phrase
+
+# Output section
+print(first_phrase)
+print(second_phrase)
+print(third_phrase)
+print(author_name)
